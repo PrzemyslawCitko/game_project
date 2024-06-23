0e3f70c231b8c7622dc9c1214bf71a5c592e0ee4
PrzemyslawCitko <przemeks1911@gmail.com>
2024-06-23 09:53:14 +0200

    added classes Coordinate nad Distance

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ from math import radians, sin, cos, atan2, sqrt
 
 COORDINATES_FILE = 'dane1.csv'
 KM_earth_R = 6371
+
+@dataclass
+class Coordinate:
+    country: str
+    capital: str
+    lon: float
+    lat: float
+
+@dataclass
+class Distance:
+    actual_country: str
+    actual_capital: str
+    closest_country: str
+    closest_capital: str
+    distance: float
