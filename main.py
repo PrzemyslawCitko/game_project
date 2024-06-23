5a727ca6f2f3c257bb845ccc0ec07deac93f28c8
PrzemyslawCitko <przemeks1911@gmail.com>
2024-06-23 09:55:28 +0200

    added func: load_coordinates

--- main.py
+++ main.py
@@ -21,3 +21,22 @@ class Distance:
     closest_country: str
     closest_capital: str
     distance: float
+
+
+def load_coordinates():
+    coordinates = []
+    try:
+        with open(COORDINATES_FILE, encoding='utf-8') as stream:
+            reader = csv.DictReader(stream)
+            for row in reader:
+                coordinate = Coordinate(
+                    country=row['country'],
+                    capital=row['capital'],
+                    lon=float(row['lon']),
+                    lat=float(row['lat']),
+                )
+                coordinates.append(coordinate)
+    except FileNotFoundError:
+        print('Nie znaleziono pliku.')
+        sys.exit(1)
+    return coordinates    
