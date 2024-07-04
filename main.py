6fed0bd6ce0753e68254c55715119af7ac1b7199
PrzemyslawCitko <przemeks1911@gmail.com>
2024-07-04 17:29:03 +0200

    removed prints

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ app = Flask(__name__)
 
 coordinates = load_coordinates()
 drawn_capital = draw_capital(coordinates)
-print(drawn_capital)
 distances = create_distances(drawn_capital, coordinates)
 closest = find_closest_capital(distances)
-print(closest)
 exclude_countries = [drawn_capital.country]
 game_over = False
 
@@ -34,25 +32,20 @@ def clicked():
     if country == closest.another_country:
         print('OK')
         exclude_countries.append(country)
-        print(exclude_countries)
         next_country = next((c for c in coordinates if c.country == closest.another_country), None)
-        print(next_country)
         if next_country:
             distances = create_distances(next_country, coordinates, exclude_countries)
-            print(distances)
             if not distances:
                 print('You won!')
                 game_over = True
             else:
                 closest = find_closest_capital(distances)
-                print(closest)
     else:
         print('Wrong answer.')
         game_over = True
 
     return jsonify({'status': 'success', 'country': country})
-
-
-
+    
+    
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=5000)
